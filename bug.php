e01ae3b9c39a7b56b3bcaca38bdf25a5a1a84156
BugHunter-X <askzenai@gmail.com>
2024-12-30 02:24:06 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,14 @@
+function foo(array $arr) {
+  foreach ($arr as $key => $value) {
+    if ($value === 'bar') {
+      unset($arr[$key]);
+    }
+  }
+  return $arr;
+}
+
+$arr = ['foo', 'bar', 'baz'];
+$result = foo($arr);
+print_r($result); // Output: Array ( [0] => foo [2] => baz )
+
+//The bug is that it modifies the array in place
